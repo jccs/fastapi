750d5d06a16eb783ac22e069002edb8a3eb0df1f
Julio Ceballos <ceballos.julio@gmail.com>
2025-08-30 18:55:53 -0500

    Added post method to create new students

--- myapi.py
+++ myapi.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,28 +9,33 @@ students = {
     1: {
         "name": "John",
         "age": 17,
-        "class": "year 12"
+        "year": "year 12"
     },
     2: {
         "name": "Kurt Cobain",
         "age": 18,
-        "class": "year 12"
+        "year": "year 12"
     },
     3: {
         "name": "Krist Novoselic",
         "age": 16,
-        "class": "year 11"
+        "year": "year 11"
     },
     4: {
         "name": "Dave Grohl",
         "age": 18,
-        "class": "year 12"
+        "year": "year 12"
     }
 }
 
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
+
 @app.get("/students/")
 def index():
-    return students#{"name": "First Data"}
+    return students #{"name": "First Data"}
 
 @app.get("/students/{student_id}")
 def get_student(student_id: int = Path(..., description="The ID of the student you want to view", gt=0, lt=5)):
@@ -41,3 +47,10 @@ def get_student_by_name(*, student_id: int, name: Optional[str]=None, test: int)
         if students[student_id]["name"] == name:
             return students[student_id]
     return {"Data": "Not Found"}
+
+@app.post("/students/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students:
+        return {"Error": "Student already exists"}
+    students[student_id] = student
+    return students[student_id]
